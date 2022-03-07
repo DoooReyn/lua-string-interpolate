975da0ca8146a00645090976bf5236f325e0aaa3
Reyn <jl88744653@gmail.com>
2022-03-07 17:00:59 +0800

    Update string_interpolate.lua

--- string_interpolate.lua
+++ string_interpolate.lua
@@ -1,14 +1,16 @@
 function string.interpolate(fmt, keys)
-    keys = type(keys) == "table" and keys or {}
     local ret =
         string.gsub(
         fmt,
         '{%w+}',
         function(c)
             local key = string.match(c, "(%w+)")
-            // 添加数字索引支持
+            -- 添加数字索引支持
             key = tonumber(key) or key
-            return keys[key] or ''
+            local val = keys[key]
+            -- 转化为字符串
+            val = tostring（val == nil and '' or val）
+            return val
         end
     )
     return ret
